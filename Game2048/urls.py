f3f81876337f80ecd9a7e87237742980fe359da3
DESKTOP-1VE9SIC\arcan <aktepearcan@gmail.com>
2021-12-28 14:29:06 +0300

    no message

--- Game2048/urls.py
+++ Game2048/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gamelogic.urls'))
-]
+    path('', include('gamelogic.urls')),
+    path("loaderio-575bf839274838b537e06b3c64230c9e.txt",
+        TemplateView.as_view(template_name="loaderio-575bf839274838b537e06b3c64230c9e.txt", content_type="text/plain"),
+    ),
+    ]
